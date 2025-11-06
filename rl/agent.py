0cda35c559b2b1466dbb2ab85fdb5ccec918d394
s1nbo <77055060+s1nbo@users.noreply.github.com>
2025-11-06 12:43:28 +0100

    init rl branch

---
+++ rl/agent.py
@@ -0,0 +1,20 @@
+# RL Agent for Catan game
+
+class Agent:
+    def __init__(self):
+        self.state = None
+        self.action_space = []
+        self.policy = {}
+    
+
+    def observe(self):
+            '''We take the json as it is send to the other players in a multiplayer game 
+            and convert it to a state representation'''
+            pass
+
+    def action(self):
+        '''Return an action based on the current policy and state'''
+        pass
+
+
+
