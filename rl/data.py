06db1b4bf51f33c522ba35847c0fa5d1a110ef11
s1nbo <77055060+s1nbo@users.noreply.github.com>
2025-11-22 18:24:31 +0100

    bit faster reading, as static board already defines board structure

--- rl/data.py
+++ rl/data.py
@@ -1,7 +1,7 @@
 import torch
 from torch_geometric.data import HeteroData
 
-from game import board
+from game import static_board
 
 class CatanData(HeteroData):
     def __init__(self):
@@ -30,9 +30,8 @@ class CatanData(HeteroData):
         }
 
         self.building_map = {
-            'None': [1, 0, 0],
-            'Settlement': [0, 1, 0],
-            'City': [0, 0, 1],
+            'Settlement': [1, 0],
+            'City': [0, 1],
         }
 
         self.owner_map = {
@@ -53,77 +52,52 @@ class CatanData(HeteroData):
             '2:1 Ore': [0, 0, 0, 0, 0, 1],
         }
 
-        self.player_id = None # Find out the player id (1-4), but the RL agent is always Player1 (self) (Needs to be mapped)
+        self._init_static_graph()
+
+        self.player_id = self.get_relative_id() # Find out the player id (1-4), but the RL agent is always Player1 (self) (Needs to be mapped)
         self.player_count = None # Number of players in the game (2-4)
-   
-        self.data = HeteroData()
-
-        self._init_static_graph(board)
 
 
 
+    def _init_static_graph(self):
 
-    def _init_static_graph(self, board: dict):
-        
-        # Tiles
-        tiles_tiles = []
-        tiles_vertices = []
-        tiles_edges = []
+        def build_edge_index(adjacency_list):
+            src, dst = [], []
+            for i, neighbors in enumerate(adjacency_list):
+                for n in neighbors:
+                    src.append(i)
+                    dst.append(n)
+            return torch.tensor([src, dst], dtype=torch.long)
+    
+        self['tile', 'T2T', 'tile'].edge_index = build_edge_index(static_board.TILE_TILE)
+        self['tile', 'T2V', 'vertex'].edge_index = build_edge_index(static_board.TILE_VERTEX)
+        self['tile', 'T2E', 'edge'].edge_index = build_edge_index(static_board.TILE_EDGE)
 
-        for t in board['tiles']:
-            tile = t['id']
-            for t in t['tiles']:
-                tiles_tiles.append((tile, t))
-            for v in t['vertices']:
-                tiles_vertices.append((tile, v))
-            for e in t['edges']:
-                tiles_edges.append((tile, e))
-        
-        self.data['tile', 'T2T', 'tile'].edge_index = torch.tensor(tiles_tiles, dtype=torch.long).t().contiguous()
-        self.data['tile', 'T2V', 'vertex'].edge_index = torch.tensor(tiles_vertices, dtype=torch.long).t().contiguous()
-        self.data['tile', 'T2E', 'edge'].edge_index = torch.tensor(tiles_edges, dtype=torch.long).t().contiguous()
+        self['vertex', 'V2T', 'tile'].edge_index = build_edge_index(static_board.VERTEX_TILE)
+        self['vertex', 'V2V', 'vertex'].edge_index = build_edge_index(static_board.VERTEX_VERTEX)
+        self['vertex', 'V2E', 'edge'].edge_index = build_edge_index(static_board.VERTEX_EDGE)
 
-        # Vertices
-        vertices_tiles = []
-        vertices_vertices = []
-        vertices_edges = []
-
-        for v in board['vertices']:
-            vertex = v['id']
-            for t in v['tiles']:
-                vertices_tiles.append((vertex, t))
-            for vv in v['vertices']:
-                vertices_vertices.append((vertex, vv))
-            for e in v['edges']:
-                vertices_edges.append((vertex, e))
-        
-        self.data['vertex', 'V2T', 'tile'].edge_index = torch.tensor(vertices_tiles, dtype=torch.long).t().contiguous()
-        self.data['vertex', 'V2V', 'vertex'].edge_index = torch.tensor(vertices_vertices, dtype=torch.long).t().contiguous()
-        self.data['vertex', 'V2E', 'edge'].edge_index = torch.tensor(vertices_edges, dtype=torch.long).t().contiguous()
-
-        # Edges
-        edges_tiles = []
-        edges_vertices = []
-        edges_edges = []
-
-        for e in board['edges']:
-            edge = e['id']
-            for t in e['tiles']:
-                edges_tiles.append((edge, t))
-            for v in e['vertices']:
-                edges_vertices.append((edge, v))
-            for ee in e['edges']:
-                edges_edges.append((edge, ee))
-
-        self.data['edge', 'E2T', 'tile'].edge_index = torch.tensor(edges_tiles, dtype=torch.long).t().contiguous()
-        self.data['edge', 'E2V', 'vertex'].edge_index = torch.tensor(edges_vertices, dtype=torch.long).t().contiguous()
-        self.data['edge', 'E2E', 'edge'].edge_index = torch.tensor(edges_edges, dtype=torch.long).t().contiguous()
-
-        
+        self['edge', 'E2T', 'tile'].edge_index = build_edge_index(static_board.EDGE_TILE)
+        self['edge', 'E2V', 'vertex'].edge_index = build_edge_index(static_board.EDGE_VERTEX)
+        self['edge', 'E2E', 'edge'].edge_index = build_edge_index(static_board.EDGE_EDGE)
 
 
     def update_from_state(self):
-        pass
+        
+        tile_features = []
+        for t in game_state['board']['tiles']:
+            tile_features.append(self._encode_tile(t))
+        self['tile'].x = torch.tensor(tile_features, dtype=torch.float)
+
+        vertex_features = []
+        for v in game_state['board']['vertices']:
+            vertex_features.append(self._encode_vertices(v))
+        self['vertex'].x = torch.tensor(vertex_features, dtype=torch.float)
+
+        edge_features = []
+        for e in game_state['board']['edges']:
+            edge_features.append(self._encode_edge(e))
+        self['edge'].x = torch.tensor(edge_features, dtype=torch.float)
 
 
     def get_relative_id(self):
@@ -146,27 +120,25 @@ class CatanData(HeteroData):
     def validate(self):
         pass
 
-    def _encode_tile(self, t): # 21 elements
+    def _encode_tile(self, t): # 20 features
         typeflag = [1, 0, 0]
-        rob = 1 if t['robber'] else 0
         resource = self.resource_map[t["resource"]]
         number = self.number_map[t["number"]]
+        rob = 1 if t['robber'] else 0
+        
         
         return typeflag + resource + number + [rob]
 
-    def _encode_vertices(self, v): # 18 elements
+    def _encode_vertices(self, v): # 15 features
         typeflag = [0, 1, 0]
-        building = self.building_map[v['building']]
         owner = self.owner_map[v['player']]
+        building = self.building_map[v['building']]
         port = self.port_map[v['port']]
+
         return typeflag + owner + building  + port
 
-    def _encode_edge(self, e): # 8 elements
+    def _encode_edge(self, e): # 7 features
         typeflag = [0, 0, 1]
         owner = self.owner_map[e['player']]
+
         return typeflag + owner
-    
-    def _encode_board(self, board: dict):
-        tile_features = []
-        vertex_features = []
-        edge_features = []
